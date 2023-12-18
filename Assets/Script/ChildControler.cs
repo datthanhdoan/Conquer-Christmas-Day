f7c101997baad8230b07b4d40daba99b1586ec02
datthanhdoan <datthanhdoan.charlie@gmail.com>
2023-12-19 02:12:33 +0700

    Add waiting queue

--- Assets/Script/ChildControler.cs
+++ Assets/Script/ChildControler.cs
@@ -1,26 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ChildControler : MonoBehaviour
 {
     public Item[] itemsResult;
+    public int currentWayPoint = 0;
     private Item wantedItem;
+    public float expectedTime;
     public Item receivedItem;
     public GameObject thinkingImage;
-    public WayPoints wayPoints;
-    private int currentWayPoint;
     public Image[] ReactionImages;
-    bool isTaken = false;
+    public SpawnerManager spawnerManager;
+    public bool isTaken { get; private set; } = false;
     bool isFacingRight = false;
     bool isWaited = false;
 
     private void Start()
     {
         //start from the first waypoint
-        transform.position = wayPoints.Points[0];
-        currentWayPoint = 0;
+
         // random the wanted item
         wantedItem = itemsResult[Random.Range(0, itemsResult.Length)];
         // set the wanted item image to the thinking image ( or popup image , whenever you want to call)
@@ -28,35 +29,39 @@ public class ChildControler : MonoBehaviour
     }
     private void Update()
     {
-        // Move to the waypoint index = 1 when the item is not taken
-        if (!isTaken)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, wayPoints.points[currentWayPoint + 1], 2 * Time.deltaTime);
-        }
-        // move to the end of the waypoint when the item is taken
-        if (isTaken && currentWayPoint < wayPoints.points.Length - 1)
-        {
-            // flip in waypoint index = 2
-            if (currentWayPoint == 2 && !isFacingRight)
-            {
-                Flip();
-            }
-            // if not wait, wait and move to the next waypoint and go to the end of the waypoint
-            if (isWaited)
-            {
-                isWaited = false;
-                if (transform.position == wayPoints.points[currentWayPoint + 1])
-                {
-                    currentWayPoint++;
-                }
-                transform.position = Vector3.MoveTowards(transform.position, wayPoints.points[currentWayPoint + 1], 2 * Time.deltaTime);
+        if (!isTaken) expectedTime -= Time.deltaTime;
+        if (expectedTime <= 0) thinkingImage.GetComponent<Image>().sprite = ReactionImages[1].sprite;
 
-            }
-            else
-            {
-                StartCoroutine(WaitAndMove());
-            }
-        }
+        // Move to the waypoint index = 1 when the item is not taken
+        // if (!isTaken)
+        // {
+        //     // transform.position = Vector3.MoveTowards(transform.position, wayPoints.points[currentWayPoint + 1], 2 * Time.deltaTime);
+        // }
+        // // move to the end of the waypoint when the item is taken
+        // if (isTaken && currentWayPoint < wayPoints.points.Length - 1)
+        // {
+        //     // flip in waypoint index = 2
+        //     if (currentWayPoint == 2 && !isFacingRight)
+        //     {
+        //         Flip();
+        //     }
+        //     // if not wait, wait and move to the next waypoint and go to the end of the waypoint
+        //     if (isWaited)
+        //     {
+        //         isWaited = false;
+        //         if (transform.position == wayPoints.points[currentWayPoint + 1])
+        //         {
+        //             currentWayPoint++;
+        //         }
+        //         transform.position = Vector3.MoveTowards(transform.position, wayPoints.points[currentWayPoint + 1], 2 * Time.deltaTime);
+
+        //     }
+        //     else
+        //     {
+        //         StartCoroutine(WaitAndMove());
+        //     }
+        // }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -92,12 +97,9 @@ public class ChildControler : MonoBehaviour
         yield return new WaitForSeconds(1.5f);
         isWaited = true;
     }
-    void Flip()
+    public void Flip()
     {
-
-        isFacingRight = true;
-        transform.Rotate(0f, 180f, 0f);
-
+        transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
     }
 }
 
---
+++ Assets/Script/SpawnerManager.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    public WayPoints santaStoreWaypoint;
+    public WayPoints homeWaypoint;
+
+    public Vector3[] waitQueue;
+    public Vector3[] homeQueue;
+    public List<GameObject> child = new List<GameObject>();
+    private List<GameObject> childHome = new List<GameObject>();
+    public GameObject childPrefab;
+    public float ingameTime;
+    private float timeSpawn;
+    private float _timerSpawn;
+    void Start()
+    {
+        waitQueue = santaStoreWaypoint.points;
+        homeQueue = homeWaypoint.points;
+        _timerSpawn = timeSpawn;
+    }
+
+    void Update()
+    {
+        ingameTime += Time.deltaTime;
+        SetTimeSpawn();
+        MoveChildToHome();
+        MoveChildToNextPoints(child, waitQueue);
+        if (_timerSpawn <= 0 && child.Count < waitQueue.Length - 1)
+        {
+            Spawn();
+            _timerSpawn = timeSpawn;
+        }
+        else if (child.Count < waitQueue.Length)
+        {
+            _timerSpawn -= Time.deltaTime;
+        }
+
+    }
+    void MoveChildToHome()
+    {
+        float speed = 2f; // Define a speed for the movement
+
+        if (child.Count > 0)
+        {
+            if (child[0].GetComponent<ChildControler>().isTaken || child[0].GetComponent<ChildControler>().expectedTime <= 0)
+            {
+                childHome.Add(child[0]);
+                child.RemoveAt(0);
+            }
+        }
+
+        for (int i = 0; i < childHome.Count; i++)
+        {
+
+            // int currentWayPoint = childHome[i].GetComponent<ChildControler>().currentWayPoint;
+            // if (childHome[i].transform.position != homeQueue[currentWayPoint])
+            // {
+            //     childHome[i].transform.position = Vector3.MoveTowards(childHome[i].transform.position, homeQueue[currentWayPoint], speed * Time.deltaTime);
+            // }
+            // else
+            // {
+            //     childHome[i].GetComponent<ChildControler>().currentWayPoint++;
+            // }
+            for (int j = 0; j < homeQueue.Length - 1; j++)
+            {
+                // ChildControler currentChild = childHome[i].GetComponent<ChildControler>();
+                if (childHome[i].transform.position == homeQueue[0]) childHome[i].GetComponent<ChildControler>().Flip();
+                if (childHome[i].transform.position != homeQueue[childHome[i].GetComponent<ChildControler>().currentWayPoint])
+                {
+                    childHome[i].transform.position = Vector3.MoveTowards(childHome[i].transform.position, homeQueue[childHome[i].GetComponent<ChildControler>().currentWayPoint], speed * Time.deltaTime);
+                }
+                else if (childHome[i].GetComponent<ChildControler>().currentWayPoint < homeQueue.Length - 1)
+                {
+                    childHome[i].GetComponent<ChildControler>().currentWayPoint++;
+                }
+            }
+        }
+    }
+    void MoveChildToNextPoints(List<GameObject> child, Vector3[] queue)
+    {
+        float speed = 2f; // Define a speed for the movement
+        // for (int i = 0; i < child.Count; i++)
+        // {
+        //     if (child[i].transform.position != waitQueue[i + 1])
+        //     {
+        //         child[i].transform.position = Vector3.MoveTowards(child[i].transform.position, queue[i + 1], speed * Time.deltaTime);
+        //     }
+        // }
+        int index = queue.Length - 1;
+        for (int i = 0; i < child.Count; i++)
+        {
+            if (child[i].transform.position != queue[index])
+            {
+                child[i].transform.position = Vector3.MoveTowards(child[i].transform.position, queue[index], speed * Time.deltaTime);
+            }
+            index--;
+        }
+    }
+    void SetTimeSpawn()
+    {
+        int timeToStore = 6;
+        if (ingameTime > 0) timeSpawn = 7 + timeToStore; Debug.Log("timeSpawn = " + timeSpawn);
+        if (ingameTime > 30) timeSpawn = 5 + timeToStore; Debug.Log("timeSpawn = " + timeSpawn);
+        if (ingameTime > 60) timeSpawn = 3 + timeToStore; Debug.Log("timeSpawn = " + timeSpawn);
+        if (ingameTime > 90) timeSpawn = 2 + timeToStore; Debug.Log("timeSpawn = " + timeSpawn);
+        if (ingameTime > 120) timeSpawn = 0 + timeToStore; Debug.Log("timeSpawn = " + timeSpawn);
+        if (ingameTime > 180) timeSpawn = -2 + timeToStore; Debug.Log("timeSpawn = " + timeSpawn);
+    }
+    void SetExpectedTime(GameObject child)
+    {
+        // child.GetComponent<ChildControler>().expectedTime = ingameTime;
+        int timeToStore = 6;
+        if (ingameTime > 0) child.GetComponent<ChildControler>().expectedTime = 10 + timeToStore;
+        if (ingameTime > 60) child.GetComponent<ChildControler>().expectedTime = 8 + timeToStore;
+        if (ingameTime > 120) child.GetComponent<ChildControler>().expectedTime = 6 + timeToStore;
+        if (ingameTime > 180) child.GetComponent<ChildControler>().expectedTime = 4 + timeToStore;
+
+    }
+    void Spawn()
+    {
+        child.Add(Instantiate(childPrefab, waitQueue[0], Quaternion.identity));
+        SetExpectedTime(child[child.Count - 1]);
+    }
+
+}
